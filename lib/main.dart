9058f573ba130ec491734526a68f5a3abd5a82a0
JH94_Surface <hhtwht75@gmail.com>
2023-08-08 05:50:26 +0900

    퀀트 테스트

--- lib/main.dart
+++ lib/main.dart
@@ -12,15 +12,39 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-            title: Text('앱')
+          title: Text('퀀트 매니저'),
+          centerTitle: true,
+          backgroundColor: Colors.orange,
         ),
-        body: Align(
-          alignment: Alignment.bottomLeft,
-          child: Container(
-            width: double.infinity, height: 150, color: Colors.blue,
+        body: Container(
+          child: Align(
+            alignment: Alignment.topCenter,
+            child: SizedBox(
+              // width: 300,
+              child: ExpansionTile(
+                title: Text("퀀트전략을 선택하세요"),
+                children: [
+                  ListTile(title: Text("GAA")),
+                  ListTile(title: Text("GAA aggressive")),
+                ],
+              ),
+            ),
           ),
         ),
-        
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.orange,
+          child: SizedBox(
+            height: 50,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Icon(Icons.arrow_back),
+                Icon(Icons.home),
+                Icon(Icons.arrow_forward),
+              ]
+            ),
+          )
+        ),
       )
     );
   }
