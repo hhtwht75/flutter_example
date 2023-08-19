944af007025d6e2dfaf42483c7b40cf3228113ee
hhtwht75 <hhtwht75@gmail.com>
2023-08-19 15:20:29 +0900

    Button test 완료

--- lib/main.dart
+++ lib/main.dart
@@ -1,118 +1,48 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var a = 1;
+  var name = ['박정훈', '신유현', '심재선', '이상경'];
+  var like = [0, 0, 0, 0];
+
   @override
   Widget build(BuildContext context) {
 
     return MaterialApp(
       home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          child: Text(a.toString()),
+          onPressed: (){
+            setState(() {
+              a++;
+            });
+          },
+        ),
         appBar: AppBar(
-          foregroundColor: Colors.green,
-          leading: Icon(Icons.attach_money_rounded),
           title: Text(
             'WHY NOT QUANT?',
-            style: TextStyle(
-              // color: Colors.black,
-              fontWeight: FontWeight.bold,
-            )
-          ),
-          // centerTitle: true,
-          backgroundColor: Colors.white,
-          actions: [
-            IconButton(onPressed: (){}, icon: Icon(Icons.menu)),
-            IconButton(onPressed: (){}, icon: Icon(Icons.add_alert))
-          ],
-        ),
-        
-        // body: Container(
-        //   padding: EdgeInsets.all(20),
-        //   decoration: BoxDecoration(border: Border.all(color: Colors.black)),
-        //   height: 250,
-        //   width: double.infinity,
-        //   child: Row(
-        //     children: [
-        //       Flexible(
-        //         flex:3,
-        //         child: Container(
-        //           margin: EdgeInsets.fromLTRB(10,0,0,0),
-        //           child: Image(
-        //             alignment: Alignment.center,
-        //             image: AssetImage('assets/wine.jpg'),
-        //           ),
-        //         ),
-        //       ),
-        //       Flexible(
-        //         flex: 7,
-        //         child: Padding(
-        //           padding: const EdgeInsets.fromLTRB(20,20,10,20),
-        //           child: Column(
-        //             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        //             crossAxisAlignment: CrossAxisAlignment.start,
-        //             children: [
-        //               Expanded(
-        //                 child: Text(
-        //                   '캐논 DSLR 100D (단렌즈, 충전기 16기가SD 포함 솰라솰라솰라솰라)',
-        //                   softWrap: true,
-        //                   // overflow: TextOverflow.clip,
-        //                 ),
-        //               ),
-        //               SizedBox(
-        //                 height: 30,
-        //                 child: Text('성동구 행당동 끌올 10분전')
-        //               ),
-        //               SizedBox(height: 30, child: Text('210,000원')),
-        //               SizedBox(
-        //                 height: 30,
-        //                 child: Row(
-        //                   mainAxisAlignment: MainAxisAlignment.end,
-        //                   children: [
-        //                     Icon(Icons.favorite_border),
-        //                     Text('4')
-        //                   ],
-        //                 ),
-        //               )
-        //             ],
-        //           ),
-        //         ),
-        //       )
-        //     ],
-        //   ),
-        // ),
-        
-        body: Align(
-          alignment: Alignment.topCenter,
-          child: Container(
-            margin: EdgeInsets.all(50),
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.black)
-            ),
-            child: ExpansionTile(
-              title: Text("퀀트전략을 선택하세요", textAlign: TextAlign.center),
-              children: [
-                ListTile(title: Text("GAA", textAlign: TextAlign.center)),
-                ListTile(title: Text("GAA aggressive", textAlign: TextAlign.center)),
-              ],
-            ),
-          ),
-        ),
-        bottomNavigationBar: BottomAppBar(
-          color: Colors.green,
-          child: SizedBox(
-            height: 50,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Icon(Icons.arrow_back),
-                Icon(Icons.home),
-                Icon(Icons.arrow_forward),
-              ]
-            ),
+            style: TextStyle(fontWeight: FontWeight.w100),
           )
+        ),
+        body: ListView.builder(
+          itemCount: 4,
+          itemBuilder: (c, i){
+            return ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text(name[i]),
+            );
+          },
         ),
       )
     );
